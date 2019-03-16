a7716d5e761559be3f49196967fe371ff1cdae37
danube88 <36491066+danube88@users.noreply.github.com>
2019-03-16 23:07:19 +0300

    Update ListComponent.vue

--- resources/js/components/ListComponent.vue
+++ resources/js/components/ListComponent.vue
@@ -1,15 +1,61 @@
 <template>
+<div>
   <div>
     <p><h2>Список сотрудников</h2></p>
   </div>
+  <div>
+    <div class="table-responsive">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Ф.И.О.</th>
+            <th>Должность</th>
+            <th>Ф.И.О. начальника</th>
+            <th>Дата рождения</th>
+            <th>Дата приема на работу</th>
+            <th>Размер заработной платы</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(worker, index) in workers">
+            <td>{{ worker.table_number }}</td>
+            <td>{{ worker.nameWorker }}</td>
+            <td>{{ worker.name_position }}</td>
+            <td>{{ worker.nameHead }}</td>
+            <td>{{ worker.birthday }}</td>
+            <td>{{ worker.reception_date }}</td>
+            <td>{{ worker.salary }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
   export default {
+    data: function () {
+      return {
+        workers: []
+      }
+    },
     mounted() {
-      this.isHierarchy = false;
-      this.isList = true;
-      console.log('Component mounted.');
+      var app = this;
+      axios.get('/api/list/data/index')
+        .then(function (resp) {
+          console.log(resp);
+          app.workers = resp.data;
+        })
+        .catch(function (resp) {
+          console.log(resp);
+          alert("Could not load workers");
+        });
+      console.log('Component workers.')
     }
   }
 </script>
+
+<style>
+</style>
